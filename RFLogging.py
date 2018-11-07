fd96ef6f4396f023148e7c24a57f3d256b94b73b
Mikko Korpela <mikko.korpela@gmail.com>
2018-11-07 17:35:33 +0200

    tech first

--- RFLogging.py
+++ RFLogging.py
@@ -1,25 +1,38 @@
 from websocket import create_connection
+import uuid
+import json
+
 c = create_connection("ws://radiant-wildwood-39558.herokuapp.com/")
+myid = str(uuid.uuid4())
+message_index = 0
+
+c.send(json.dumps([myid, message_index]))
+message_index += 1
 
 ROBOT_LISTENER_API_VERSION = 2
 
 def start_suite(name, attributes):
-    c.send("--- START SUITE '%s' ---" % name)
+    _send("--- START SUITE '%s' ---" % name)
+
+def _send(data):
+    global message_index
+    c.send(json.dumps([myid, message_index, data]))
+    message_index += 1
 
 def end_suite(name, attributes):
-    c.send("--- END SUITE '%s' --- %s " % (name, attributes["status"]))
+    _send("--- END SUITE '%s' --- %s " % (name, attributes["status"]))
 
 def start_test(name, attributes):
-    c.send("--- START TEST '%s' ---" % name)
+    _send("--- START TEST '%s' ---" % name)
 
 def end_test(name, attributes):
-    c.send("--- END TEST '%s' --- %s " % (name, attributes["status"]))
+    _send("--- END TEST '%s' --- %s " % (name, attributes["status"]))
 
 def start_keyword(name, attributes):
-    c.send("--- START KEYWORD '%s' ---" % name)
+    _send("--- START KEYWORD '%s' ---" % name)
 
 def end_keyword(name, attributes):
-    c.send("--- END KEYWORD '%s' --- %s " % (name, attributes["status"]))
+    _send("--- END KEYWORD '%s' --- %s " % (name, attributes["status"]))
 
 def log_message(message):
-    c.send(message["message"])
+    _send(message["message"])
