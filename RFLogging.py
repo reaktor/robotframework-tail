e40b7a84caadcefc963e8dba88006fad49ffc090
Mikko Korpela <mikko.korpela@gmail.com>
2018-11-08 15:58:45 +0200

    more style

--- RFLogging.py
+++ RFLogging.py
@@ -12,30 +12,30 @@ message_index += 1
 ROBOT_LISTENER_API_VERSION = 2
 
 def start_suite(name, attributes):
-    _send("--- START SUITE '%s' ---" % name)
+    _send("START", "SUITE '%s'" % name)
 
-def _send(data):
+def _send(command, data):
     global message_index
-    c.send(json.dumps([myid, message_index, data]))
+    c.send(json.dumps([myid, message_index, command, data]))
     message_index += 1
 
 def end_suite(name, attributes):
-    _send("--- END SUITE '%s' --- %s " % (name, attributes["status"]))
+    _send("END", "SUITE '%s' - %s" % (name, attributes["status"]))
 
 def start_test(name, attributes):
-    _send("--- START TEST '%s' ---" % name)
+    _send("START", "TEST '%s' ---" % name)
 
 def end_test(name, attributes):
-    _send("--- END TEST '%s' --- %s " % (name, attributes["status"]))
+    _send("END", "TEST '%s' --- %s " % (name, attributes["status"]))
 
 def start_keyword(name, attributes):
-    _send("--- START KEYWORD '%s' ---" % name)
+    _send("START", "KEYWORD '%s' ---" % name)
 
 def end_keyword(name, attributes):
-    _send("--- END KEYWORD '%s' --- %s " % (name, attributes["status"]))
+    _send("END", "KEYWORD '%s' --- %s " % (name, attributes["status"]))
 
 def log_message(message):
-    _send(message["message"])
+    _send("MESSAGE", message["message"])
 
 def close():
-    _send("--- CLOSE ---")
+    _send("CLOSE", "CLOSE")
