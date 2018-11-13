612a7d131ceb6cb2430b75ab8dcbb8223b87e614
Mikko Korpela <mikko.korpela@gmail.com>
2018-11-13 10:08:58 +0200

    login page

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+const server = express();
+server.use('/', express.static('public'));
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
 
 const wss = new SocketServer({ server });
 
